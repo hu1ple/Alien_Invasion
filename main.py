baf865a5b751917fe9247983d0af1a57b13407c3
hu1ple <1581310028@qq.com>
2023-09-13 10:59:57 +0800

    add ship setting class and paint the ship

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# 这是一个示例 Python 脚本。
-
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
-
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+import sys
+import pygame
+from Setting import Setting
+from ship import Ship
 
 
-# 按间距中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def run_game():
+    """初始化游戏并创建一个屏幕对象"""
+    pygame.init()
+    setting = Setting()
+    screen = pygame.display.set_mode((setting.screen_width, setting.screen_height))
+    # 创建一艘飞船
+    ship = Ship(screen)
+    pygame.display.set_caption("Alien Invasion")
 
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+    # 开始游戏的主循环
+    while True:
+        # 监视键盘和鼠标事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        # 每次循环时都重绘屏幕
+        screen.fill(setting.bg_color)
+        ship.blitme()
+        # 让最近绘制的屏幕可见
+        pygame.display.flip()
+
+
+run_game()
