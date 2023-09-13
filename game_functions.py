1322c3977e65013bd6a69e1769c8ee8993b4f218
hu1ple <1581310028@qq.com>
2023-09-13 20:55:41 +0800

    一行外星人

--- game_functions.py
+++ game_functions.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from bullet import Bullet
+from Alien import Alien
 
 
 def check_keydown_events(event, ship, ai_settings, bullets, screen):
@@ -19,7 +20,7 @@ def fire_bullet(ai_settings, bullets, screen, ship):
     if len(bullets) < ai_settings.bullet_allowed:
         new_bullet = Bullet(ai_settings, screen=screen, ship=ship)
         bullets.add(new_bullet)
- 
+
 
 def check_keyup_events(event, ship):
     """响应松开"""
@@ -39,6 +40,7 @@ def check_events(ship, screen, ai_settings, bullets):
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
+
 def update_bullets(bullets):
     bullets.update()
     # 删除已经消失的子弹
@@ -47,12 +49,32 @@ def update_bullets(bullets):
             bullets.remove(bullet)
 
 
-def update_screen(ai_settings, screen, ship, bullets):
+def create_fleet(ai_settings, screen, aliens):
+    """创建外星人群"""
+    # 创建一个外星人，并计算一行可容纳多少个外星人
+    # 外星人的间距为外星人的宽度
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_alien_x = int(available_space_x / (2 * alien_width))
+
+    #创建第一行外星人
+    for alien_number in range(number_alien_x):
+        # 创建一个外星人并将其加入到当前行
+        alien = Alien(ai_settings, screen)
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        aliens.add(alien)
+
+
+def update_screen(ai_settings, screen, ship, bullets,aliens):
     # 每次循环时都重绘屏幕
     screen.fill(ai_settings.bg_color)
     # 在飞船和外星人后面重绘所有子弹
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
+    for alien in aliens.sprites():
+        alien.blitme()
     # 让最近绘制的屏幕可见
     pygame.display.flip()
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from Setting import Setting
 from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
+from Alien import Alien
 
 
 def run_game():
@@ -16,11 +17,15 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
     # 创建一个用于存储子弹的编组
     bullets = Group()
-
+    # 创建外星人群
+    aliens = Group()
+    gf.create_fleet(setting, screen, aliens)
     # 开始游戏的主循环
     while True:
         gf.check_events(ship, ai_settings=setting, bullets=bullets, screen=screen)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(setting, screen, ship, bullets)
+        gf.update_screen(setting, screen, ship, bullets,aliens)
+
+
 run_game()
